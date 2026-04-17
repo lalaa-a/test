9236a51e8672579ba4645b95198b59d104d4df50
Abhijeeth2003 <155721362+Abhijeeth2003@users.noreply.github.com>
2026-04-18 01:36:44 +0530

    just comitt

--- app/views/Admin/Moderator/moderator.php
+++ app/views/Admin/Moderator/moderator.php
@@ -1,25 +1,13 @@
 <!-- Compact Navigation Bar -->
 <nav class="content-nav">
     <ul class="nav-tabs">
-        <li class="nav-tab">
-            <button class="nav-tab-link" data-tab="subtabAllModerators">
-                <i class="fa-solid fa-id-card"></i>
-                <span class="nav-tab-text">All Moderators</span>
-            </button>
-        </li>
-        <li class="nav-tab">
-            <button class="nav-tab-link active" data-tab="subtabSiteModerators">
-                <i class="fa-solid fa-user"></i>
-                <span class="nav-tab-text">Site Moderators</span>
-            </button>
-        </li>
-        <li class="nav-tab">
-            <button class="nav-tab-link" data-tab="subtabBusinessModerators">
-                <i class="fa-solid fa-id-card"></i>
-                <span class="nav-tab-text">Business Moderators</span>
-            </button>
-        </li>
 
+        <li class="nav-tab">
+            <button class="nav-tab-link active" data-tab="subtabModeratorVerification">
+                <i class="fa-solid fa-user-shield"></i>
+                <span class="nav-tab-text">Moderator Verifications</span>
+            </button>
+        </li>
     </ul>
 </nav>
 
